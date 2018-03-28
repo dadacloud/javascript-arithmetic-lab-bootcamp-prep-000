c7a0de10c74906a5e7b9e5e9b78ef8965fbd0cf5
Marc Jean-Gilles <marcjeangilles@gmail.com>
2018-03-28 21:03:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ function add(a,b){
   return (a + b);
 }
 
-function 
+function subtract (a,b){
+  return a-b
+}
+
+function multiply (a,b){
+  return a*b
+}
+
+function divide(a,b){
+  return a/b
+}
+
